4a093c2adfea9cfcff5774d9330b88ddbc30fedc
EFTEKHER <eftekherali2000@gmail.com>
2022-06-18 22:26:09 +0600

    dart exam

---
+++ 1-dart-types-example.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  //Number
+  var x=10;
+  print(x.runtimeType);
+  var y=10.34;
+  print(y.runtimeType);
+  String e="Efte";
+  print(e.runtimeType);
+  var details = new Map(); 
+   details['name'] = 'Efte'; 
+   details['address'] = 'Dakshin khan'; 
+   print(details);
+   print(details.runtimeType);
+   
+}
